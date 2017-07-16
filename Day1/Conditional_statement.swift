05a0c5b80dfc63fbec351c6edcee07e691d7d3da
codyowl <codyowl@gmail.com>
2017-07-16 22:58:16 +0530

    Conditional statement (if) added :heavy_plus_sign:

---
+++ Day1/Conditional_statement.swift
@@ -0,0 +1,22 @@
+/* if :
+   syntax : if expression { //note: the braces should comes after expression
+                     thigns to do
+                    }
+*/
+
+var Message = "Hai"
+
+if Message == "Hai" {
+    print ("Yes message is Hai !")
+}
+
+// if and else
+
+var Second_message = "Bye"
+
+if Second_message == "Hai"{
+    print ("second message is Hai !")
+}
+else {
+    print ("second message is not hai")
+}
