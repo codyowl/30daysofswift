3f76d5e3639cec5e80b2d1172b20a15622ad23d9
codyowl <codyowl@gmail.com>
2017-07-17 23:07:23 +0530

    Loops(while, for, repeate) added inside day 2 :heavy_plus_sign:

---
+++ Day2/Loops.swift
@@ -0,0 +1,48 @@
+//While
+
+/* syntax: 
+while condition {
+    statement
+} */
+
+
+var i = 1
+
+while i <= 10 { 
+    print (i) 
+    i = i + 1 
+}
+    
+//--------------------------------------------------------------------------
+//repeat
+/*
+syntax:
+repeat {
+	statement
+}while condition
+
+*/
+
+var j = 1
+
+repeat {
+	print (j)
+	j = j + 1
+} while j < 10
+
+//---------------------------------------------------------------------------
+//for loop
+/*
+syntax :
+for value in range {
+	statement
+} */
+
+print ("Printing inside for loop")
+for data in 1...10{
+	print (data)
+}
+
+
+
+
