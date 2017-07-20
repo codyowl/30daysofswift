1a1ae583548d7c4d024c351bfb071f1a0afb8df4
codyowl <codyowl@gmail.com>
2017-07-20 10:32:45 +0530

    Introduction to class added :heavy_plus_sign:

---
+++ Day4/Class.swift
@@ -0,0 +1,17 @@
+/*syntax to create a class : 
+class <your_class_name> {
+
+}
+*/
+
+class Firstclass{
+        func first_function(my_argument:String) {
+            print (my_argument)
+        }
+}
+
+var my_first_instance = Firstclass()
+
+var my_argument = "text inside my argument"
+
+my_first_instance.first_function(my_argument:my_argument)
