bb3e33a99111af787fe3b1b2fd2646109d91e72e
codyowl <codyowl@gmail.com>
2017-07-27 10:44:16 +0530

    Day7 Properties added :heavy_plus_sign:

---
+++ Day7/Properties.swift
@@ -0,0 +1,8 @@
+/* properties are nothing but the variables inside a class or structure.Any instance can access that particular class' variable*/
+
+class MyFirstClass {
+    var my_first_variable = 2
+}
+
+var my_instance = MyFirstClass()
+print ("\(my_instance.my_first_variable)") //note to print an integer values we are need of a back slash along with the varaible
